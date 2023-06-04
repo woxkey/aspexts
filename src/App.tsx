d001b12730b8089310dda7f9f02c52f233172c67
Alen <bolatovalen@gmail.com>
2023-06-04 22:01:12 +0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = (): React.ReactElement => {
+
+    return (
+            <div>
+                hello world
+            </div>
+    )
+}
+
+export default App
